503364d2abd58233898938b7587289df561737fe
phil@home <philipp.millner@outlook.com>
2024-01-09 15:01:34 +0100

    Version 1.0.8 release

--- obj/Release/net8.0/Console_Crawler.AssemblyInfo.cs
+++ obj/Release/net8.0/Console_Crawler.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Console_Crawler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c552296281f73eb4d0bebc6395ef79d09df2a06f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+076f3d212689eea2a742932d9f6542d434521251")]
 [assembly: System.Reflection.AssemblyProductAttribute("Console_Crawler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Console_Crawler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
